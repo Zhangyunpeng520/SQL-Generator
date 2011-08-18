3b238c737e9da5c19839ef0cae8d496c6a706b65
Daniel Martuszewski <daniel10a@o2.pl>
2011-08-18 22:46:03 +0200

    Some changes

--- class.Config.php
+++ class.Config.php
@@ -3,7 +3,7 @@
 class Config {
 
     public static $dbUser = 'daniel';
-    public static $dbPassword = 'drealine';
+    public static $dbPassword = '';
     public static $dbDNS = 'mysql:dbname=testbaza;host=localhost';
 
 }
